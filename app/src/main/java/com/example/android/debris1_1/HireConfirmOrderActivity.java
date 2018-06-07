83aa8e6b2f4724b19a68f97c3eddd15f98bc605e
patri-m <patrick_6by20@hotmail.co.uk>
2018-06-07 13:47:40 +0100

    Confirm order java began, creations of payment and congratulations activities

--- app/src/main/java/com/example/android/debris1_1/HireConfirmOrderActivity.java
+++ app/src/main/java/com/example/android/debris1_1/HireConfirmOrderActivity.java
@@ -2,12 +2,37 @@ package com.example.android.debris1_1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class HireConfirmOrderActivity extends AppCompatActivity {
+
+    TextView price;
+    Button termsAndConditionsButton;
+    Button chooseCollectionDateButton;
+    Button goToPaymentButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hire_confirm_order);
+
+        price = findViewById(R.id.total_price_confirm_order);
+        termsAndConditionsButton = findViewById(R.id.terms_and_conditions_check_box_confirm_order);
+        chooseCollectionDateButton = findViewById(R.id.button_choose_pick_up_date_before_ordering_confirm_order);
+        goToPaymentButton = findViewById(R.id.button_to_payment_page_confirm_order);
+
+        String priceString = "£" + Control.CONTROL.getCurrentOrder().getPrice() + "0*";
+        price.setText(priceString);
+
+        //If the user has already chosen their collection date, the button to do so is removed
+        //and some text on the page is changed.
+        if(Control.CONTROL.getCurrentOrder().getCollectionDateSpecified()){
+            chooseCollectionDateButton.setVisibility(View.GONE);
+            goToPaymentButton.setText("");
+        }
+
+
     }
 }
---
+++ app/src/main/java/com/example/android/debris1_1/HirePaymentActivity.java
@@ -0,0 +1,13 @@
+package com.example.android.debris1_1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HirePaymentActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hire_payment);
+    }
+}
